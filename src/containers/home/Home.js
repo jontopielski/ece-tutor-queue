5d33e5ef2c4f1b0825f440a2ea183efd1b8742ee
Jesse Ren <jeren.neurogen@gmail.com>
2017-10-28 17:43:49 -0700

    Adding Tutor Page

---
+++ src/containers/home/Home.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { push } from 'react-router-redux'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import {
+  increment,
+  incrementAsync,
+  decrement,
+  decrementAsync
+} from '../../modules/counter'
+
+import "./Home.css"
+
+const Home = props => (
+  <div className="Home" >
+    <h1>ECE Tutoring Queue</h1>
+    <p className="Home-queue"> {props.count} People in Line</p>
+    <p>
+      <button onClick={props.increment} disabled={props.isIncrementing}>Increment</button>
+      <button onClick={props.incrementAsync} disabled={props.isIncrementing}>Increment Async</button>
+    </p>
+
+    <p>
+      <button onClick={props.decrement} disabled={props.isDecrementing}>Decrementing</button>
+      <button onClick={props.decrementAsync} disabled={props.isDecrementing}>Decrement Async</button>
+    </p>
+
+    <p><button onClick={() => props.changePage()}>Go to about page via redux</button></p>
+  </div>
+)
+
+const mapStateToProps = state => ({
+  count: state.counter.count,
+  isIncrementing: state.counter.isIncrementing,
+  isDecrementing: state.counter.isDecrementing,
+  tickets: state.counter.tickets
+})
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  increment,
+  incrementAsync,
+  decrement,
+  decrementAsync,
+  changePage: () => push('/about-us')
+}, dispatch)
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home)
